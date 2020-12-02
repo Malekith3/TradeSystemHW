b72a678dbe112c6aab79650508b3e5e4c43187c1
Malekith3 <sanek33333@gmail.com>
2020-12-02 15:54:34 +0200

    Premake added and UML fixed

---
+++ Premake5.lua
@@ -0,0 +1,43 @@
+workspace "TradeSystemHW1"
+	architecture "x64"
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "TradeSystem"
+	location "TradeSystem"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+
+	 files 
+	 { 
+	 	"%{prj.name}/sorce/Headers/**.h",
+	 	"%{prj.name}/sorce/CPP/**.cpp"
+	 }
+
+	 includedirs
+	 {
+		"TradeSystem/sorce"
+	 }
+
+	filter "system:windows"
+	  cppdialect "C++17"
+	  staticruntime "On"
+	  systemversion "latest"
+      defines { "_CRT_SECURE_NO_WARNINGS" }
+      symbols "On"
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols  "On"
+ 
+    filter "configurations:Release"
+      defines { "NDEBUG" }
+      symbols  "On"
+      optimize "On"
+
