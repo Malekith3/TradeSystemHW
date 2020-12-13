003c25ba7f1cf5ea42feec523eaf0934250202d7
Malekith3 <sanek33333@gmail.com>
2020-12-13 15:06:33 +0200

    Small Fix

--- TradeSystem/source/initializtion.cpp
+++ TradeSystem/source/initializtion.cpp
@@ -11,5 +11,5 @@ void Initialization(TradeSystem& tradeSystem)
 	tradeSystem.AddSeller(Seller("Pipin", "23456", Address("Nowhere", 5, "Tel Aviv", "Earth")));
 	tradeSystem.GetSeller("Pipin").AddProduct(Product("Notebook", 56, Product::Office));
 	tradeSystem.GetSeller("Pipin").AddProduct(Product("PS5", 400, Product::Electronics));
-	tradeSystem.GetBuyer("Pipin").AddToShoppingCart(Product("Axe", 1000, Product::Office));
+	tradeSystem.GetSeller("Pipin").AddProduct(Product("Axe", 1000, Product::Office));
 }
